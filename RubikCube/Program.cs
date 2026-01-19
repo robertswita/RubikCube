551a62d3f14087e979b34075c5c3c8ac167be310
Kosikowski <8352262+Kosikowski@users.noreply.github.com>
2026-01-19 13:02:05 +0000

    Add high DPI support for proper control scaling

--- RubikCube/Program.cs
+++ RubikCube/Program.cs
@@ -14,6 +14,7 @@ namespace RubikCube
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TRubikForm());
