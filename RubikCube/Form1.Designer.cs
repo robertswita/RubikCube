7111ed80f03fdc9447818e44b8e8c7d778e6ea90
Kosikowski <8352262+Kosikowski@users.noreply.github.com>
2026-01-19 12:34:14 +0000

    Fix control docking order for proper layout

--- RubikCube/Form1.Designer.cs
+++ RubikCube/Form1.Designer.cs
@@ -763,10 +763,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1202, 818);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.tglView1);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tglView1);
+            this.Controls.Add(this.pictureBox1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "TRubikForm";
             this.Text = "Rubik\'s Cube";
