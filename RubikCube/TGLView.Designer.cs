71ea830f7c9ad65b6f589769e4ddaecd0ec0dd0c
Kosikowski <8352262+Kosikowski@users.noreply.github.com>
2026-01-19 12:13:17 +0000

    Replace TGLView with OpenTK GLControl

--- RubikCube/TGLView.Designer.cs
+++ RubikCube/TGLView.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TGL
+namespace TGL
 {
     partial class TGLView
     {
@@ -23,13 +23,12 @@
         #region Component Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify 
+        /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         }
 
         #endregion
--- RubikCube/TGLView.cs
+++ RubikCube/TGLView.cs
@@ -1,45 +1,32 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
+using OpenTK.WinForms;
+using OpenTK.Graphics.OpenGL4;
 
 namespace TGL
 {
-    public partial class TGLView : UserControl
+    public partial class TGLView : GLControl
     {
-        // W konstruktorze kontrolki tworzymy klasę kontekstu TGLContext i przypisujemy jej uchwyt zwrotny do kontrolki. 
-        // Polecenie new alokuje obiekt na stosie zarządzanym przez Garbage Collector. 
-        // Takiego obiektu nie musimy sami dealokować.
         public TGLContext Context = new TGLContext();
+
         public TGLView()
         {
             InitializeComponent();
             ResizeRedraw = true;
-            SetStyle(ControlStyles.Opaque, true);
             Context.View = this;
         }
-        // Kontrolka rysuje swoje wnętrze w odpowiedzi na komunikat okna WM_PAINT. 
-        // Nadpiszemy handler tego komunikatu - OnPaint i wywołamy w nim metodę DrawScene kontekstu:
+
         protected override void OnPaint(PaintEventArgs e)
         {
-            if (Context.Handle != IntPtr.Zero)
-                Context.DrawView();
+            base.OnPaint(e);
+            MakeCurrent();
+            Context.DrawView();
+            SwapBuffers();
         }
 
         public void Recreate()
         {
-            RecreateHandle();
+            // Not needed with GLControl
         }
-
-        // Kontekst DC kontrolki będzie kontekstem prywatnym, tworzonym razem z oknem kontrolki
-        // i zwalnianym dopiero przy niszczeniu okna. Trzeba ustalić styl klasy okna na CS_OWNDC.
-        protected override CreateParams CreateParams
-        {
-            get
-            {
-                CreateParams cp = base.CreateParams;
-                cp.ClassStyle |= (int)Win32.CS_OWNDC;
-                return cp;
-            }
-        }
-    };
-
+    }
 }
